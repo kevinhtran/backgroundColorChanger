c0bb21dfca6e04457316b240ee5e711ad29e3f85
kevinhtran <kevdeveloper7@gmail.com>
2020-04-02 18:08:01 -0700

    created a function to get a random color generated with click event

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-const changeColor = document.getElementById("changeColor"), // get element by the changeColor id
-      colors = ['#FFBD66', '#EF617B', '#D3485D', '#475677', '#283150']; // color choices
-let colorIndex = 0; // index of the current color
+// const changeColor = document.getElementById("changeColor"), // get element by the changeColor id
+//       colors = ['#FFBD66', '#EF617B', '#D3485D', '#475677', '#283150']; // color choices
+// let colorIndex = 0; // index of the current color
 
-changeColor.addEventListener("click", () => { // listen for a click event
-  document.body.style.backgroundColor = colors[colorIndex];  // change color to the current color index 
-  colorIndex = (colorIndex + 1) % colors.length // increment colorIndex by 1 and if it goes beyond the length of the array, then get it back to 0 using modulo %
+// changeColor.addEventListener("click", () => { // listen for a click event
+//   document.body.style.backgroundColor = colors[colorIndex];  // change color to the current color index 
+//   colorIndex = (colorIndex + 1) % colors.length // increment colorIndex by 1 and if it goes beyond the length of the array, then get it back to 0 using modulo %
+// });
+
+
+// Color Gradient
+const changeColor = document.getElementById("changeColor");
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+changeColor.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomColor();
 });
