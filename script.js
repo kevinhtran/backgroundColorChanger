9cd6917ebbebd38c5e5c9a0f8f673aae3b6d8681
kevinhtran <kevdeveloper7@gmail.com>
2020-04-07 23:43:41 -0700

    creating a structure formanipulating the DOM with fetch api

--- script.js
+++ script.js
@@ -18,3 +18,35 @@ fetch('https://reqres.in/api/users' {
   })
   .then(data => console.log(data))
   .catch(error => console.log('ERROR'))
+
+
+// Create an event listener for the DOM after it loads
+document.addEventListener('DOMContentLoaded', () => {
+  // place all your code here
+})
+
+// Step 2: Create function that will run the fetch request function
+function fetchData() {
+
+}
+
+// Step 3: Write the fetch request
+
+function fetchData() {
+  fetch('http://localhost3000/quotes'); // is this the correct link?
+}
+
+// Step 4: Convert fetch promise into JSON
+
+function fetchData() {
+  fetch('http://localhost:3000/quotes')
+  .then( response => response.json() )
+}
+
+// Step 5: Send JSON data into a callback function
+
+function fetchData() {
+  fetch('http://localhost:3000/quotes')
+  .then( response => response.json() )
+  .then( data => some_function(data))
+}
